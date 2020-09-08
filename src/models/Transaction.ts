4ff9d364bf4252bc1bc64dcfe8c9d373caefb130
savio <saviopf22@gmail.com>
2020-09-08 15:50:52 -0300

    criação da entidade e especificações do typeorm nos models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,40 @@
+import {
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToMany,
+  JoinColumn,
+  Entity,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('number')
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToMany(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
